a58e42c9c99c65c38b57ec7f204056aa864638b6
Mishajj39 <kotliar.m00@gmail.com>
2024-10-02 10:23:15 +0200

    Update main.cpp

--- labs/lab2/main.cpp
+++ labs/lab2/main.cpp
@@ -9,7 +9,6 @@ int main()
     cin >> R;
     double pi = 3.141592653;
     double S = pi * R * 2;
-    cout << "S = ";
-    printf("%.9f", S);
+    cout << fixed << setprecision(9) << "S = " << S;
     return 0;
 }
